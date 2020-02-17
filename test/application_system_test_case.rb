cd1c40817b2832972b50a8e63072b697194b26c1
Maysam Torabi <kherad@gmail.com>
2020-02-17 14:27:32 +0330

    we are not using selenium

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,9 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :rack_test
+  Capybara.default_driver = :rack_test
+  setup do
+    stub_request(:get, /chromedriver.storage.googleapis.com/)
+      .to_return(status: 200, body: '', headers: {})
+  end
 end

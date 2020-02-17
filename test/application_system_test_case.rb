0847a9b249694c16dd232b9f9dfd4ec42034852e
Maysam Torabi <kherad@gmail.com>
2020-02-17 14:07:45 +0330

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,18 +4,18 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :rack_test
-  
+  setup do
     stub_request(:get, %r|chromedriver.storage.googleapis.com|)
       .to_return(status: 200, body: '', headers: {})
   
-stub_request(:get, "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_80.0.3987").
-  with(
-    headers: {
-	  'Accept'=>'*/*',
-	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-	  'Host'=>'chromedriver.storage.googleapis.com',
-	  'User-Agent'=>'Ruby'
-    }).
-  to_return(status: 200, body: "", headers: {})
-
+	stub_request(:get, "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_80.0.3987").
+	  with(
+	    headers: {
+		  'Accept'=>'*/*',
+		  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+		  'Host'=>'chromedriver.storage.googleapis.com',
+		  'User-Agent'=>'Ruby'
+	    }).
+	  to_return(status: 200, body: "", headers: {})
+  end
 end

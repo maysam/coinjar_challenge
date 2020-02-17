67640047222bf8f02747cc6868bf87b3e3f61e77
Maysam Torabi <kherad@gmail.com>
2020-02-17 13:34:34 +0330

    Update challenge_test.rb

--- test/system/challenge_test.rb
+++ test/system/challenge_test.rb
@@ -4,8 +4,20 @@ require 'application_system_test_case'
 
 class ChallengeTest < ApplicationSystemTestCase
   setup do
-    stub_request(:get, /chromedriver.storage.googleapis.com/)
+    stub_request(:get, %r|chromedriver.storage.googleapis.com|)
       .to_return(status: 200, body: '', headers: {})
+    
+stub_request(:get, "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_80.0.3987").
+  with(
+    headers: {
+	  'Accept'=>'*/*',
+	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+	  'Host'=>'chromedriver.storage.googleapis.com',
+	  'User-Agent'=>'Ruby'
+    }).
+  to_return(status: 200, body: "", headers: {})
+
+    
   end
 
   test 'homepage has latest prices for each currency' do

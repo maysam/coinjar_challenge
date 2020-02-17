be8dfe846ad0166c2497c834a0e974cd4cea42b2
Maysam Torabi <kherad@gmail.com>
2020-02-17 16:11:56 +0330

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+WebMock.allow_net_connect!
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by(:rack_test, using: :rack_test)
-  # driven_by :rack_test, using: nil
-  setup do    
-    stub_request(:get, %r|chromedriver.storage.googleapis.com|)
-      .to_return(status: 200, body: '', headers: {})
-  
-	stub_request(:get, "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_80.0.3987").
-	  with(
-	    headers: {
-		  'Accept'=>'*/*',
-		  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-		  'Host'=>'chromedriver.storage.googleapis.com',
-		  'User-Agent'=>'Ruby'
-	    }).
-	  to_return(status: 200, body: "", headers: {})
-  end
+  driven_by :rack_test
 end
